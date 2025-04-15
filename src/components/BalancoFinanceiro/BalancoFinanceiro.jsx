bea93696dbab8564b039e7252846e32cac5ccd4a
GGhiaroni <gghiaronitiziano@gmail.com>
2025-04-15 10:12:54 -0300

    adicionando o chartjs no projeto

--- src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
+++ src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
@@ -1,7 +1,28 @@
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Tooltip,
+} from "chart.js";
+import { observer } from "mobx-react";
+import { Pie } from "react-chartjs-2";
+import { useStoreContext } from "src/mobx/StoreContext";
+import styled from "styled-components";
 import Cartao from "../Cartao/Cartao";
 import CartaoCabecalho from "../Cartao/CartaoCabecalho/CartaoCabecalho";
 import CartaoCorpo from "../Cartao/CartaoCorpo/CartaoCorpo";
-import styled from "styled-components";
+
+ChartJS.register(
+  ArcElement,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Tooltip,
+  Legend
+);
 
 export const AreaChart = styled.div`
   padding: var(--padding-xs);
@@ -9,14 +30,54 @@ export const AreaChart = styled.div`
   height: 50%;
 `;
 
-const BalancoFinanceiro = () => {
+const BalancoFinanceiro = observer(() => {
+  const { transacoesStore } = useStoreContext();
+
+  const categoriasAgrupadas = transacoesStore.gastosPorCategoria;
+
+  const data = {
+    labels: Object.keys(categoriasAgrupadas),
+    datasets: [
+      {
+        label: "Gastos por categoria",
+        data: Object.values(categoriasAgrupadas),
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "right",
+      },
+    },
+  };
+
   return (
     <Cartao>
       <CartaoCabecalho>Gastos por categoria</CartaoCabecalho>
       <CartaoCorpo>
-        <AreaChart></AreaChart>
+        <AreaChart>
+          <Pie data={data} options={options} />
+        </AreaChart>
       </CartaoCorpo>
     </Cartao>
   );
-};
+});
 export default BalancoFinanceiro;
